9f31c99c4f8c7c68ca0b66fa77807b3840aac620
hankai <hankai@sparksoft.com.cn>
2018-11-27 17:39:39 +0800

    为面包屑导航拦截器增加单元测试。

---
+++ source/cordwood-core/src/test/java/ren/hankai/cordwood/web/breadcrumb/BreadCrumbInterceptorTest.java
@@ -0,0 +1,26 @@
+
+package ren.hankai.cordwood.web.breadcrumb;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import ren.hankai.cordwood.core.test.CoreTestSupport;
+
+public class BreadCrumbInterceptorTest extends CoreTestSupport {
+
+  @Test
+  public void testPreHandle() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testPostHandle() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testAfterCompletion() {
+    fail("Not yet implemented");
+  }
+
+}
