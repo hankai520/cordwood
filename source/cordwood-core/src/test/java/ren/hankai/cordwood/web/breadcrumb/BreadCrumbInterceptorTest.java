c0b3e327ab62095d456c38dc947aa7a2d9bb61b0
hankai <hankai@sparksoft.com.cn>
2018-11-27 17:44:37 +0800

    临时提交，测试服务器端checkstyle验证。

--- source/cordwood-core/src/test/java/ren/hankai/cordwood/web/breadcrumb/BreadCrumbInterceptorTest.java
+++ source/cordwood-core/src/test/java/ren/hankai/cordwood/web/breadcrumb/BreadCrumbInterceptorTest.java
@@ -6,6 +6,13 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 import ren.hankai.cordwood.core.test.CoreTestSupport;
 
+/**
+ * TODO Missing type description
+ *
+ * @author hankai
+ * @version 1.0.0
+ * @since Nov 27, 2018 5:44:07 PM
+ */
 public class BreadCrumbInterceptorTest extends CoreTestSupport {
 
   @Test
